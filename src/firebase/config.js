7f8776015b76ae7fe5ca8d09e1f97bf7f1477940
Mateusz Domagała <kontakt@mateuszdomagala.pl>
2021-08-18 17:40:10 +0200

    [Finishes #3]Task: Add Firebase to the project (#6)
    # Story Title
    
    [Task: Add Firebase to the project](https://github.com/mateuszdomagala/vue-invoice-app/issues/3)
    
    ## Changes made
    
    * feat: add Firebase to the project
    * ci: add a GitHub workflow to deploy to Firebase hosting
    
    ## How does the solution address the problem
    
    This PR will add Firebase for authentication and data management and integrate deploys to Firebase Hosting via a GitHub Action.
    
    ## Linked issues
    
    Resolves #3

---
+++ src/firebase/config.js
@@ -0,0 +1,19 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+  authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_FIREBASE_APP_ID,
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const firestore = firebase.firestore();
+const auth = firebase.auth();
+
+export { firestore, auth };
