84eefeccb74caf466d36708d70728bf6512e2c11
Mateusz Domagała <kontakt@mateuszdomagala.pl>
2021-09-27 20:51:05 +0200

    [Finishes #35]Task: Create a Not Found View (#38)
    # Story Title
    
    [Task: Create a Not Found View](https://github.com/mateuszdomagala/vue-invoice-app/issues/35)
    
    ## Changes made
    
    feat: add Not Found view
    
    ## How does the solution address the problem
    
    This PR will add the not found view used for non-existent routes.
    
    ## Linked issues
    
    Resolves #35

---
+++ src/views/NotFound.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    <img src="../assets/illustration-empty.svg" alt="no invoices" />
+    <h2>This page could not be found</h2>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
+  padding: 30px;
+
+  margin: 150px auto 0 auto;
+
+  @media (min-width: 900px) {
+    margin: 0 auto;
+  }
+}
+</style>
